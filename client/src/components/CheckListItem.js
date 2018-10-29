f2cd11e0c78148b050f3ed6879aaeb54f234dab5
Dennis Osipov <dennis.osipov@surveycircle.com>
2018-10-29 02:42:32 +0100

    added list item to fill out and add to list

--- client/src/components/CheckListItem.js
+++ client/src/components/CheckListItem.js
@@ -5,14 +5,26 @@ function CheckListItem(props) {
     return (
         <ListGroupItem className='checklist__item'>
             
-            <label className="checkbox" htmlFor={ props._id }>
-                <input type="checkbox" defaultChecked={ props.status === 'DONE' ? 'checked' : ''} value="" id={ props._id } data-toggle="checkbox" className="custom-checkbox" onChange={ () => props.handleChange }/>
-                <span className="icons"><span className="icon-unchecked"></span><span className="icon-checked"></span></span>
-                {/* label text */}
+            <div className='icon'>
+                <label className="checkbox" htmlFor={ props._id }>
+                    <input type="checkbox" defaultChecked={ props.status === 'DONE' ? 'checked' : ''} value="" id={ props._id } data-toggle="checkbox" className="custom-checkbox" onChange={ () => props.handleChange }/>
+                    <span className="icons"><span className="icon-unchecked"></span><span className="icon-checked"></span></span>
+                    {/* label text */}
                 </label>
+            </div>
             <div className='textfield'>{ props.description }</div>
         </ListGroupItem>
     );
 }
 
-export default CheckListItem;
+function CheckListItemTemplate(props) {
+    return (
+        <ListGroupItem className='checklist__item'>
+            <div className='icon'>
+                <span className="fui-plus icon"></span>
+            </div>
+            <input type='text' className='textfield custom-control' placeholder='Add new task'/>
+        </ListGroupItem>
+    );
+}
+export { CheckListItem, CheckListItemTemplate };
